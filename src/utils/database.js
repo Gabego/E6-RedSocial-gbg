5cb36e0d2ccc97da5f0976e346a34e65ebc1a96a
Gabriel Bello <gabego@yahoo.com>
2022-12-21 21:46:45 -0500

    Production version

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,7 +7,17 @@ const db = new Sequelize({
     username: config.db.user,
     password: config.db.pass,
     database: config.db.name,
-    port: config.db.port
+    port: config.db.port,
+    dialectOptions:                         //?solo para produccion
+    process.env.NODE_ENV === 'production'
+    ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    } : {}
+
+
 })
 
 
